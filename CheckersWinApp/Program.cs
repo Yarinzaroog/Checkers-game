9c9237d8042aa9f4eba41dc38c075c1c8acf480a
Yarinzaroog <140937093+Yarinzaroog@users.noreply.github.com>
2025-02-11 11:56:17 +0200

    Add files via upload

---
+++ CheckersWinApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CheckersWinApp
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            CheckersApplication app = new CheckersApplication();
+
+            app.Run();
+        }
+    }
+}
